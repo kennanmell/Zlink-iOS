e22eed5311878f8a845719f67dde70ad2544f985
Kennan Mell <kmell96@cs.washington.edu>
2016-10-25 22:40:34 -0700

    Fixed date bug

--- Zlink/Calendar.swift
+++ Zlink/Calendar.swift
@@ -20,9 +20,9 @@ struct Calendar {
     static func getCurrentDate() -> String {
         let date = Date()
         let calendar = Foundation.Calendar.current
-        let day = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.day, from: date).day)
-        let month = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.month, from: date).month)
-        let year = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.year, from: date).year)
+        let day = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.day, from: date).day!)
+        let month = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.month, from: date).month!)
+        let year = String(describing: (calendar as NSCalendar).components(NSCalendar.Unit.year, from: date).year!)
         return month + "/" + day + "/" + year
     }
     
